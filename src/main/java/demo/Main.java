9c20f401a749cd1714a9ed72a01d360954ec3897
Sudharsan B <25sudharsan27@gmail.com>
2023-12-28 12:15:57 +0530

    spring boot learning

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,8 +1,18 @@
 package demo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-    public static void main(String[] args){
-        Doctor doctor=new Doctor();
-        doctor.assist();
+    public static void main(String[] args) {
+        // Load the Spring XML configuration file
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        // Retrieve the bean from the context
+        // Nurse doctor = (Nurse) context.getBean("nurse");
+        Doctor staff = context.getBean(Doctor.class);
+        staff.assist();
+        System.out.println(staff.getQualification());
+        // Use the bean
+        // ...
     }
 }
