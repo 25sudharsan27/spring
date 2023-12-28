ed030dd2d006dff637478676dd5e13e7a2846876
Sudharsan B <117500265+25sudharsan27@users.noreply.github.com>
2023-12-28 05:20:41 +0000

    modified

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,5 +1,7 @@
 package demo;
 
 public class Doctor {
-    
+    public void assist(){
+        System.out.println("Doctor is assisting");
+    }
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -3,5 +3,6 @@ package demo;
 public class Main {
     public static void main(String[] args){
         Doctor doctor=new Doctor();
+        doctor.assist();
     }
 }
